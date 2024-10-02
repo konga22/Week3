0f6cd3f64e83c752b2ee374bb40f4aeb3cd87fad
konga22 <qwopop2@gmail.com>
2024-10-02 14:44:29 +0900

    Initial commit

--- src/Question13.java
+++ src/Question13.java
@@ -13,10 +13,44 @@ public class Question13 {
         char u2 = sc.next().charAt(0);
         System.out.println("3번째 숫자: ");
         int n3 = sc.nextInt();
-        System.out.println("다음에 들어갈 연산자( + - * / ) : ");
-        char u3 = sc.next().charAt(0);
         sc.close();
-        double end = 0;
+        double end = 0;//u1 최종
+        double end2 = 0;//u1 중간값
+        double endd = 0;
+        double end22 =0;
+        double end4 =0;
+
+        switch (u1){
+            case '*':
+                    end2 = n1*n2;
+                    break;
+            case '/':
+                end2 = n1/n2;
+                break;
+            case '+':end = end2 + n3;
+                break;
+            case '-':end = end2 - n3;
+                break;
+            default:
+                System.out.println("잘못한 계산");//end 가 u1최종 결과값
+        }
+        switch (u2){
+            case '*':
+                end22 = n2*n3;
+                break;
+            case '/':
+                end22 = n2/n3;
+                break;
+            case '+':endd = end22 + n1;
+                break;
+            case '-':endd = end22 - n1;
+                break;
+            default:
+                System.out.println("잘못한 계산");//end 가 u1최종 결과값
+        }
+
+        end4 = endd + end;
+        System.out.println("계산결과: "+end4);
 
 
     }
